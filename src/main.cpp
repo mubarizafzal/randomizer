395b422a732e31b1d84e2be84aec4150de75ccd2
Mubariz Afzal <mubarizafzal@gmail.com>
2020-10-21 10:43:32 -0400

    added slip string func, removing input whitespace

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+
+vector<string> splitString (const string& s, const string& delimiters) {
+  vector<string> ret;
+  string current = "";
+  bool hit = false;
+
+  for (const char &c : s) {
+    
+    for (const char &d : delimiters) {
+    
+      if (c == d) {
+        hit = true;
+        break;
+      }
+    }
+
+    if (hit) {
+      if (current != "") {
+        ret.push_back(current);
+        current = "";
+      }
+    } else {
+      current = current + c;
+    }
+
+    hit = false;
+  }
+
+  if (current != "") {
+    ret.push_back(current);
+  }
+  
+  return ret;
+}
+
 int main () {
 
-  string a;
+  string temp;
 
   // hard delimit on commas
   // we can reomve leading and trailing whitespaces
@@ -14,12 +50,24 @@ int main () {
   // ask for more input at beginning
   // (otherwise press enter without any input, or whitespace only) which starts the program
 
+  vector<string> values;
+
   while (true) {
-    if (cin >> a) {
-      cout << a << endl;
-    } else {
-      break;
+    getline(cin, temp);
+    values = splitString(temp, ",");
+    for (auto&& s: values) {
+      for (int i = 0; i < s.size(); i++) {
+        if (s[i] != ' ') {
+          s = s.substr(i, s.size() - i);
+          break;
+        }
+      }
+      while (s.back() == ' ') {
+        s.pop_back();
+      }
+      cout << s << endl;
     }
+    cout << "---------" << endl;
   }
 
 
